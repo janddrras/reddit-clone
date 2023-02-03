6749b156a7afec32a414dc1149ce36e66e0f379e
jandras <git+janddrras@users.noreply.github.com>
2023-02-03 12:00:53 +0200

    basic setup

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../navbar/Navbar'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  )
+}
+export default Layout
