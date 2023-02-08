b18b0c1aef244fa8426cd81ffb53580de40394b4
jandras <git+janddrras@users.noreply.github.com>
2023-02-08 14:02:32 +0200

    community modal ui

--- src/components/navbar/directory/Communities.tsx
+++ src/components/navbar/directory/Communities.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import CreateCommunityModal from '@/components/modals/community/CreateCommunityModal'
+import { Flex, MenuItem, Text, Icon } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { GrAdd } from 'react-icons/gr'
 
 type CommunitiesProps = {}
 
 const Communities: React.FC<CommunitiesProps> = () => {
-  return <div>Have a good coding</div>
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <CreateCommunityModal open={open} handleClose={() => setOpen(false)} />
+      <MenuItem width='100%' _hover={{ bg: 'blue.500', color: 'white' }} onClick={() => setOpen(true)}>
+        <Flex align='center'>
+          <Icon as={GrAdd} fontSize={20} mr={3} />
+          <Text fontSize='10pt'>Create Community</Text>
+        </Flex>
+      </MenuItem>
+    </>
+  )
 }
 export default Communities
